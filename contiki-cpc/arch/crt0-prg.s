355605ad1d14be70390a95f035b616110062a265
PulkoMandy <pulkomandy@pulkomandy.tk>
2014-06-28 23:20:12 +0200

    Initialize data sections for PRGs.

--- contiki-cpc/arch/crt0-prg.s
+++ contiki-cpc/arch/crt0-prg.s
@@ -1,16 +1,39 @@
-;; File: crt0-dsc.s
+;; File: crt0-prg.s
 
     .module crt0
 	.area _HEADER (ABS)
 	.area _HOME (REL)	
 	.area _CODE (REL)
+	.area _INITIALIZER
+	.area   _GSINIT (REL)
+    .area   _GSFINAL (REL)
 	;; relocation data
 	.dw 0
-     .area _DATA (REL)
+	
+	.area _INITIALIZED
+    .area _DATA (REL)
+	.area _BSEG
     .area   _BSS (REL)
     .area   _HEAP (REL)
 
 	.area   _GSINIT (REL)
 gsinit::	
+	ld	bc, #l__INITIALIZER
+	ld	a, b
+	or	a, c
+	jr	Z, gsinit_next
+	ld	de, #s__INITIALIZED
+	ld	hl, #s__INITIALIZER
+	ldir
+gsinit_next:
+; Clear BSS sections
+	ld hl,#s__DATA
+	ld (hl),#0
+	ld de,#s__DATA
+	inc de
+	ld bc,#l__DATA
+	ldir
+
+	
     .area   _GSFINAL (REL)
 	ret
